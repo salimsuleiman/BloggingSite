a09fb4ebb89c28e8ddcca8dddc62f13da6453ba1
salim <salimsuleimankt@gmail.com>
2021-05-16 19:59:15 +0100

    Complete requirements.txt and added procfile

--- project/main.py
+++ project/main.py
@@ -13,7 +13,7 @@ import os
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SECRET_KEY'] = os.environ.get("DATABASE_URL")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 
 ckeditor = CKEditor(app)
